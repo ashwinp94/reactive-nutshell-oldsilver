1c24f39992011ab9c5acd0054733aa0a60fe0116
Ashwin Prakash <ashp94@gmail.com>
2019-01-30 11:57:33 -0600

    working on getting only logged in user entries to show up

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -3,22 +3,23 @@ import React, { Component } from 'react'
 
 export default class NewsList extends Component {
     render() {
-        const sortedNewsItems = [].concat(this.props.newsitems)
-                            .sort((a,b) => {return new Date(a.newsDate).getTime() - new Date(b.newsDate).getTime()})
-                            .reverse()
-                            .map(newsitem =>
-                                    <div key={newsitem.id}>
-                                    <h3>{newsitem.title}</h3>
-                                    <h4>{newsitem.url}</h4>
-                                    <h5>{newsitem.synopsis}</h5>
-                                    <h6>{newsitem.newsDate}</h6>
-                                    <button type="button"
-                                            onClick = {() => this.props.deleteNews(newsitem.id)}
-                                            className="btn btn-success">
-                                        Delete
-                                    </button>
-                                    </div>
-                            )
+        const sortedNewsItems =
+              [].concat(this.props.newsitems)
+                .sort((a,b) => {return new Date(a.newsDate).getTime() - new Date(b.newsDate).getTime()})
+                .reverse()
+                .map(newsitem =>
+                        <div key={newsitem.id}>
+                        <h3>{newsitem.title}</h3>
+                        <h4>{newsitem.url}</h4>
+                        <h5>{newsitem.synopsis}</h5>
+                        <h6>{newsitem.newsDate}</h6>
+                        <button type="button"
+                                onClick = {() => this.props.deleteNews(newsitem.id)}
+                                className="btn btn-success">
+                            Delete
+                        </button>
+                        </div>
+                )
         return (
             <React.Fragment>
                 <div className="newsButton">
